1b08dee1dfa17508a0a89e57671805f541390876
sivasankaranarayanan-ds <sivasankaranarayanan.k@datasirpi.com>
2024-02-12 10:27:11 +0530

    variables

---
+++ variables.dart
@@ -0,0 +1,39 @@
+String empName = "Mathew"; //Variable defined outside the method
+
+void main() {
+//Declaring Variables
+  String name = "Scott";
+  String address = "Los Angles";
+  int rank = 45;
+  num age = 20; // used to store any types of numbers
+  num height = 5.2;
+  bool isMarried = false;
+  const pi = 3.14; // To create a constant in Dart, you can use the const keyword.
+  // not possible to change the pi = 4.23, because constant value never changed.
+
+// printing variables value
+  print("Name is $name");
+  print("Address is $address");
+  print("Rank is $rank");
+  print("Age is $age");
+  print("Height is $height");
+  print("Married Status is $isMarried");
+  print("Name is $empName"); // You can also access the variable which is defined outside the method
+  print("Value of PI is $pi");
+}
+
+//Variable Definition:
+// Variables are containers used to store value in the program.
+//There are different types of variables where you can keep different kinds of values.
+//Here is an example of creating a variable and initializing it.
+
+// Variable Types:
+// They are called data types. We will learn more about data types later in this dart tutorial.
+
+// String: For storing text value. E.g. “John” [Must be in quotes]
+// int: For storing integer value. E.g. 10, -10, 8555 [Decimal is not included]
+// double: For storing floating point values. E.g. 10.0, -10.2, 85.698 [Decimal is included]
+// num: For storing any type of number. E.g. 10, 20.2, -20 [both int and double]
+// bool: For storing true or false. E.g. true, false [Only stores true or false values]
+// var: For storing any value. E.g. ‘Bimal’, 12, ‘z’, true
+
