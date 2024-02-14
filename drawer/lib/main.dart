2427e33dcc39c7c09d2e62c866f3a607f53c21cd
sivasankaranarayanan-ds <sivasankaranarayanan.k@datasirpi.com>
2024-02-14 17:14:50 +0530

    drawer

---
+++ drawer/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Drawer"),
+          backgroundColor: Colors.blue,
+        ),
+        drawer: Drawer(
+          elevation: 16.0,
+          child: Column(children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              accountName: Text('Xavier'),
+              accountEmail: Text('xavier@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+            )
+          ]),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
